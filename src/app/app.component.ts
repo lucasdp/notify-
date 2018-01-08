cb73f3308783a5939a4dc8e1614cb446b7f28e7a
Lucas Del Pozzi <lucas@codetry.io>
2018-01-08 15:22:03 -0300

    Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
+  import { NativeNotificationService } from 'angular-notice/lib/native-notification.service';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
-}
+  @Component({
+  	selector: 'app-root',
+  	templateUrl: './app.component.html',
+  	styleUrls: ['./app.component.css']
+  })
+
+  export class AppComponent {
+  	title = 'app';
+
+  	constructor(private _service: NativeNotificationService) {}
+
+	notifyme(){
+	const options =  { 
+	                title: 'Hola mundo',
+	                body : 'Esta es una notificacion',
+	                icon: '../assets/imagenes/notif.png',
+	                tag: 'notice',
+	                closeDelay: 2000
+	            };
+	this._service.notify(options);
+	}
+
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+ import { BrowserModule } from '@angular/platform-browser';
+ import { NgModule } from '@angular/core';
+ import { AppComponent } from './app.component';
+ import { NotificationsModule } from 'angular-notice'
+ 
+ @NgModule({
+ 	declarations: [
+ 	AppComponent
+ 	],
+ 	imports: [
+ 	BrowserModule,
+ 	NotificationsModule
+ 	],
+ 	providers: [],
+ 	bootstrap: [AppComponent]
+ })
+ export class AppModule { }
